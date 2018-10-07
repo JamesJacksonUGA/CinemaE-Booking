78e21e1206926e1be86cfb52cf9e67e20062f8d0
Jared Adams <jared.adams26@uga.edu>
2018-10-07 18:41:07 -0400

    Creating spring boot project

---
+++ cinema_e_booking/src/main/java/cinema/booking/CinemaEBookingApplication.java
@@ -0,0 +1,12 @@
+package cinema.booking;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CinemaEBookingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CinemaEBookingApplication.class, args);
+	}
+}
